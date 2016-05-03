c4c5dbfcaffb370350c174f7b3bdd14d3e3697f2
Aya Sharaf Elden <ayasharafelden707@gmail.com>
2016-05-03 12:40:46 +0200

    delete

--- rails_project/db/schema.rb
+++ rails_project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429110343) do
+ActiveRecord::Schema.define(version: 20160502213926) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
