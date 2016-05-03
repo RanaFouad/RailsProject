fe8c195d57edf6cbc83426f27e6dd54ffdfabc12
Rana <eng.ranafouad.2013@gmail.com>
2016-05-03 15:43:36 +0200

    user updates

--- rails_project/db/schema.rb
+++ rails_project/db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 20160429183415) do
 ActiveRecord::Schema.define(version: 20160502213926) do
 
   create_table "friendships", force: :cascade do |t|
@@ -76,7 +75,6 @@ ActiveRecord::Schema.define(version: 20160502213926) do
     t.string   "menuimage_content_type", limit: 255
     t.integer  "menuimage_file_size",    limit: 4
     t.datetime "menuimage_updated_at"
-    t.string   "friends",                limit: 255
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
